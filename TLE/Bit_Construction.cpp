3fd3ef168866ae77d8492720881f94b001f8d9e1
Tanjiro <guptamanu409@gmail.com>
2024-04-23 10:56:20 +0530

    code

--- TLE/Bit_Construction.cpp
+++ TLE/Bit_Construction.cpp
@@ -16,39 +16,50 @@ int main(){
 
         long long p = k;
         long long ans = 0;
-        vector<long long>an;
+        // vector<long long>an;
         while(p){
-            if(p&1){
-                an.push_back((long long)(pow(2,ans)));
-            }
+            // if(p&1){
+                // an.push_back((long long)(pow(2,ans)));
+            // }
             ans++;
             p = p>>1;
         }
         if(n==1)cout<<k<<'\n';
-        else if(n>=an.size()){
-            for(auto &it:an)cout<<it<<" ";
-            long long rem = n-an.size();
-            while(rem){
+        else{
+            ans--;
+            long long first = k-((pow(2,ans))-1);
+            cout<<first<<" "<<k-first<<" ";
+            n = n-2;
+            while(n){
                 cout<<0<<" ";
-                rem--;
+                n--;
             }
             cout<<'\n';
         }
-        else{
-            long long cnt = 0;
-            long long i = an.size()-1;
-            while(cnt<n-1){
-                ++cnt;
-                cout<<an[i]<<" ";
-                i--;
-            }
-            long long sum = 0;
-            while(i>=0){
-                sum+=an[i];
-                i--;
-            }
-            cout<<sum<<'\n';
-        }
+        // else if(n>=an.size()){
+        //     for(auto &it:an)cout<<it<<" ";
+        //     long long rem = n-an.size();
+        //     while(rem){
+        //         cout<<0<<" ";
+        //         rem--;
+        //     }
+        //     cout<<'\n';
+        // }
+        // else{
+        //     long long cnt = 0;
+        //     long long i = an.size()-1;
+        //     while(cnt<n-1){
+        //         ++cnt;
+        //         cout<<an[i]<<" ";
+        //         i--;
+        //     }
+        //     long long sum = 0;
+        //     while(i>=0){
+        //         sum+=an[i];
+        //         i--;
+        //     }
+        //     cout<<sum<<'\n';
+        // }
 
         // cout<<k<<" ";
         // for(int i = 0; i<n-1; i++)cout<<0<<" ";
---
+++ TLE/Same_Map_RPG.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int h,w;
+    cin>>h>>w;
+    // h++;
+    // w++;
+    // vector<vector<string>>a(h,vector<string>(w));
+    // vector<vector<string>>b(h,vector<string>(w));
+
+    vector<string>a;
+    vector<string>b;
+    a.push_back("");
+
+    for(int i = 1; i<=h; i++){
+        string s;
+        for(int j = 1; j<=w; j++){
+            char x;
+            cin>>x;
+            s.push_back(x);
+        }
+        a.push_back(s);
+    }
+    b.push_back("");
+    for(int i = 1; i<=h; i++){
+        string s;
+        for(int j = 1; j<=w; j++){
+            char x;
+            cin>>x;
+            s.push_back(x);
+        }
+        b.push_back(s);
+    }
+    
+    // for(int i = 1; i<=h; i++){
+    //     for(int j = 1; j<=w; j++){
+    //         cin>>b[i][j];
+    //     }
+    // }
+
+    for(int s = 0; s<h; s++){
+        for(int t = 0; t<w; t++){
+            cout<<s<<" "<<t<<'\n';
+            bool check = true;
+            for(int i = 1; i<=h; i++){
+                for(int j = 1; j<=w; j++){
+                    int r = (i+s)%h;
+                    if(r==0)r = h;
+                    int c = (j+t)%w;
+                    if(c==0)c = w;
+                    if(a[r][c]!=b[i][j]){
+                        check = false;  
+                        break;
+                    }
+                }
+                if(!check)break;
+            }
+            if(check){
+                cout<<"Yes"<<'\n';
+                return 0;
+            }
+        }
+    }
+
+    cout<<"No"<<'\n';
+    return 0;
+}
